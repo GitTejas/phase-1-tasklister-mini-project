3b5aba1ff6b21b7e6dd821e6c42a7cd8fb93f808
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-25 21:53:08 -0500

    updating

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      buildToDo(e.target.create_item.value)
+      form.reset()
+  })
+})
+
+
+function buildToDo(todo){
+  console.log(todo)
+  let list = document.createElement('li')
+  let btn = document.createElement("button")
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+
+}
